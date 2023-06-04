4f7b3a18ac9ba0062a03bbffaeb60b88bf526b4d
ManojNadar <manojviru03@gmail.com>
2023-06-04 10:35:40 +0530

    remove Duplicate

---
+++ DS/insert.js
@@ -0,0 +1,20 @@
+let arr = [5, 20, 30, 40, 80];
+
+// console.log(arr);
+// console.log(arr.splice(1, 2, 2000)); // it will show the deleted element from an array
+// console.log(arr);
+
+let newItem = 2000;
+let position = 2;
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  if (i >= position) {
+    arr[i + 1] = arr[i];
+    console.log(arr[i]);
+  }
+  if (i === position) {
+    arr[i] = newItem;
+  }
+}
+
+console.log(arr);
---
+++ DS/removeDuplicates.js
@@ -0,0 +1,37 @@
+// let arr = ["scale", "happy", "strength", "peace", "happy", "happy"];
+
+// function removeDuplicates(arr) {
+//   return arr.filter((item, index) => arr.indexOf(item) === index); // filter duplicate values and remove it
+// }
+
+// console.log(removeDuplicates(arr));
+
+// let arr = ["scale", "happy", "strength", "peace", "happy", "happy"];
+
+// function removeDuplicates(arr) {
+//   let unique = [];
+//   arr.forEach((element) => {
+//     if (!unique.includes(element)) {
+//       // if unique array doesn't includes the same value it stores that value
+//       unique.push(element);
+//     }
+//   });
+//   return unique;
+// }
+
+// console.log(removeDuplicates(arr));
+
+let arr = ["scale", "happy", "strength", "peace", "happy", "happy"];
+
+function removeDuplicate(arr) {
+  let unique = [];
+  let dup = arr.reduce((acc, num) => {
+    if (!unique.includes(num)) {
+      unique.push(num);
+      return acc;
+    }
+  });
+  return unique;
+}
+
+console.log(removeDuplicate(arr));
