e6bb4a0f314ca0f115b274055ab1e74b7a37b3ba
ManojNadar <manojviru03@gmail.com>
2023-06-05 10:18:22 +0530

    testPractice

--- Algorithms/fibonacci.js
+++ Algorithms/fibonacci.js
@@ -1,18 +1,35 @@
 // Fibonacci
 
-function Fibonacci(n) {
-  let fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-    // fib[2] = fib[2 - 1] + fib[2 - 2];
+// function Fibonacci(n) {
+//   let fib = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     fib[i] = fib[i - 1] + fib[i - 2];
+//     // fib[2] = fib[2 - 1] + fib[2 - 2];
+//   }
+
+//   return fib;
+// }
+
+// console.log(Fibonacci(3));
+// console.log(Fibonacci(4));
+// console.log(Fibonacci(5));
+
+// // Big-O = O(n) - linear
+// as the n value increases the loop increases
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
   }
 
-  return fib;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(Fibonacci(3));
-console.log(Fibonacci(4));
-console.log(Fibonacci(5));
+console.log(fibonacci(8));
 
-// Big-O = O(n) - linear
-// as the n value increases the loop increases
+
+// Test the fibonacci function
+// const n = 10;
+// for (let i = 0; i <= n; i++) {
+//   console.log(fibonacci(i));
+// }
---
+++ testPractice/test.js
@@ -0,0 +1,91 @@
+// add three numbers sum find the target
+
+// var array = [54, 33, 55, 77, 99, 66, 44, 5, 2, 6, 7, 9, 11];
+// var target = 171;
+
+// function sumThree(array, target) {
+//   var flag = false;
+//   for (let i = 0; i < array.length - 2; i++) {
+//     for (let j = i + 1; j < array.length - 1; j++) {
+//       for (k = j + 1; k < array.length; k++) {
+//         if (array[i] + array[j] + array[k] === target) {
+//           console.log(array[i], array[j], array[k], "=", target);
+//           flag = true
+//    return flag = true
+//if we return the flag = true.then it will return the first possible output and end the loop
+//         }
+//       }
+//     }
+//   }
+//   if (flag === false) {
+//     console.log("Sorry no matches found");
+//   }
+// }
+
+// sumThree(array, target);
+
+// subtraction of three number and find target
+
+// var array = [54, 33, 55, 77, 99, 66, 44, 5, 2, 6, 7, 9, 11];
+// var target = 17;
+
+// function sumThree(array, target) {
+//   var flag = false;
+//   for (let i = 0; i < array.length - 2; i++) {
+//     for (let j = i + 1; j < array.length - 1; j++) {
+//       for (k = j + 1; k < array.length; k++) {
+//         if (array[i] - array[j] - array[k] === target) {
+//           console.log(array[i], array[j], array[k], "=", target);
+//           flag = true;
+//           //   return flag = true
+//           //if we return the flag = true.then it will return the first possible output and end the loop
+//         }
+//       }
+//     }
+//   }
+//   if (flag === false) {
+//     console.log("Sorry no matches found");
+//   }
+// }
+
+// sumThree(array, target);
+
+// find even number of given array
+
+// var array = [2, 4, 8, 10, 6, 3, 4, 79, 99, 55, 33];
+// var array2 = [8, 10, 6, 3, 4, 79, 99, 55, 33];
+
+// function findEvenNum(even) {
+//   let newEven = [];
+//   for (let i = 0; i < even.length; i++) {
+//     if (even[i] % 2 == 0) {
+//          console.log(newEven)
+//       newEven.push(even[i]);
+//     }
+//   }
+//   //   console.log(newEven);
+//   return newEven.length;
+// }
+
+// var e = findEvenNum(array2);
+// console.log(e);
+
+// find odd number
+
+// var array = [2, 4, 8, 10, 6, 3, 4, 44, 79, 99, 55, 33];
+// // var array2 = [8, 10, 6, 3, 4, 79, 99, 55, 33];
+
+// function oddNum(odd) {
+//   let newOdd = [];
+//   for (let i = 0; i < odd.length; i++) {
+//     if (odd[i] % 2 != 0) {
+//       console.log(odd[i]);
+//       newOdd.push(odd[i]);
+//     }
+//   }
+
+//   return newOdd.length;
+// }
+
+// let o = oddNum(array);
+// console.log("length of odd num is =", o);
