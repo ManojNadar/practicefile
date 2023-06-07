8c33911de607b25352075b8f846d4b7fc89672c4
ManojNadar <manojviru03@gmail.com>
2023-06-07 10:17:39 +0530

    practice array ds

--- DS/insert.js
+++ DS/insert.js
@@ -3,18 +3,21 @@ let arr = [5, 20, 30, 40, 80];
 // console.log(arr);
 // console.log(arr.splice(1, 2, 2000)); // it will show the deleted element from an array
 // console.log(arr);
+function insertElement(arr) {
+  let newItem = 2000;
+  let position = 2;
 
-let newItem = 2000;
-let position = 2;
-
-for (let i = arr.length - 1; i >= 0; i--) {
-  if (i >= position) {
-    arr[i + 1] = arr[i];
-    console.log(arr[i]);
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (i >= position) {
+      arr[i + 1] = arr[i];
+      console.log(arr[i]);
+    }
+    if (i === position) {
+      arr[i] = newItem;
+    }
   }
-  if (i === position) {
-    arr[i] = newItem;
-  }
+  return arr;
 }
 
-console.log(arr);
+let newArr = insertElement(arr);
+console.log(newArr);
---
+++ DS/traversingArray.js
@@ -0,0 +1,51 @@
+var arr = [235, 234, 212, 35, 35, 52, 23, 324, 52, 3, 5535, 35, 32, 34];
+
+// method 1
+
+// function traversing(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// traversing(arr);
+
+// method 2
+
+// function traversing(arr) {
+//   for (item of arr) {
+//     console.log(item);
+//   }
+// }
+
+// traversing(arr);
+
+// methods 3
+
+// function traversing(arr) {
+//   let newArr = arr.map((item) => {
+//     console.log(item);
+//   });
+// }
+
+// traversing(arr);
+
+// method 4
+
+// function traversing(arr) {
+//   arr.forEach((item) => {
+//     console.log(item);
+//   });
+// }
+
+// traversing(arr);
+
+// print array[elements] index
+
+// function traversing(arr) {
+//   for (item in arr) {
+//     console.log(item);
+//   }
+// }
+
+// traversing(arr);
