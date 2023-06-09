a281238ebc25094f9faee1c79aded50a1e9db144
ManojNadar <manojviru03@gmail.com>
2023-06-09 21:09:49 +0530

    practice

--- leetCode/problem-2.js
+++ leetCode/problem-2.js
@@ -1,13 +1,51 @@
 // remove Duplicate number form the array and sort the array ascending
 
-let arr = [3, 2, 9, 8, 9, 2];
+// var nums = [3, 2, 2, 3];
+// var val = 3;
 
-let newArr = [...new Set(arr)];
-// console.log(newArr);
+// function removeDuplicate(nums, val) {
+//   let k = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] ==+ val) {
+//       // console.log(nums[i]);
+//       k.push(nums[i]);
+//     }
+//   }
+//   return k;
+// }
+// let output = removeDuplicate(nums, val);
+// console.log(output);
 
-newArr.sort((a, b) => {
-  //   debugger;
-  return a - b;
-});
+// *************************************
+// var nums = [3, 2, 2, 3];
+// var val = 3;
 
-console.log(newArr);
+// function removeDuplicate(nums, val) {
+//   let k = [];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] < nums[i + 1]) {
+//       k.push(nums[i]);
+//     }
+//   }
+
+//   return k;
+// }
+// let output = removeDuplicate(nums, val);
+// console.log(output);
+
+var nums = [3, 2, 2, 3];
+var val = 3;
+
+function removeDuplicate(nums, val) {
+  let k = [];
+  for (let i = 0; i < nums.length-1; i++) {
+    if (nums[i] != nums[i + 1]) {
+      nums[i] = nums[i + 1];
+      k.push(nums[i]);
+    }
+  }
+  return k;
+}
+let output = removeDuplicate(nums, val);
+console.log(output);
