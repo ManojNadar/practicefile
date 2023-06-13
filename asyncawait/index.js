72c8e0d12ec10d1bc8893492e544474f32f38548
ManojNadar <manojviru03@gmail.com>
2023-06-13 19:50:46 +0530

    async

--- asyncawait/index.js
+++ asyncawait/index.js
@@ -19,26 +19,52 @@
 
 // Async Await
 
+async function prom() {
+  let one = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("promise 1"); // execute 2nd
+    }, 6000);
+  });
+
+  let two = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("promise 2"); //execute last
+    }, 5000);
+  });
+  //   console.log("Waiting for First Promise.....");
+  //   console.log("First Promise got...");
+  let promOne = await one;
+  console.log(`${promOne} 6000ms`);
+  //   console.log("Second Promise waiting...");
+  let promTwo = await two;
+  console.log(`${promTwo} 5000ms`);
+  //   console.log("Second Promise got");
+}
+
+function normal() {
+  console.log("iam normal");
+}
+
+async function bothPromise() {
+  normal(); //execute 1st
+  await prom();
+}
+
+bothPromise();
+
 // async function prom() {
-//   let one = new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve("promise 1");
-//     }, 5000);
-//   });
+//   setTimeout(() => {
+//     console.log("promise 1");
+//   }, 3000);
 
 //   let two = new Promise((resolve) => {
 //     setTimeout(() => {
 //       resolve("promise 2");
-//     }, 7000);
+//     }, 4000);
 //   });
-//   //   console.log("Waiting for First Promise.....");
-//   //   console.log("First Promise got...");
-//   let promOne = await one;
-//   console.log(`${promOne} 5000ms`);
-//   //   console.log("Second Promise waiting...");
+
 //   let promTwo = await two;
-//   console.log(`${promTwo} 7000ms`);
-//   //   console.log("Second Promise got");
+//   console.log(`${promTwo} await 3000ms`);
 // }
 
 // function normal() {
@@ -51,33 +77,3 @@
 // }
 
 // bothPromise();
-
-async function prom() {
-  setTimeout(() => {
-    console.log("promise 1");
-  }, 3000);
-
-  let two = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("promise 2");
-    }, 4000);
-  });
-  //   console.log("Waiting for First Promise.....");
-  //   console.log("First Promise got...");
-  // console.log(`${one} sync`);
-  //   console.log("Second Promise waiting...");
-  let promTwo = await two;
-  console.log(`${promTwo} await 3000ms`);
-  //   console.log("Second Promise got");
-}
-
-function normal() {
-  console.log("iam normal");
-}
-
-async function bothPromise() {
-  normal();
-  await prom();
-}
-
-bothPromise();
