f1624cfdd8897f65d20efa22b79fa1c1e7d27890
ManojNadar <manojviru03@gmail.com>
2023-06-19 22:37:50 +0530

    practice update

---
+++ 19-06.js
@@ -0,0 +1,33 @@
+// function outerFunction() {
+//   var myName = "manoj";
+//   console.log("outerFunction");
+//   function innerFunction() {
+//     console.log("innerFunction", myName);
+//   }
+
+//   return innerFunction;
+// }
+
+// const res = outerFunction();
+// console.log(res);
+
+// var array = [33, 34, 2, 45, 67, 7, 24, 6];
+
+// array.splice(initial value, delete Element, add element )
+
+// array.splice(2, 1, 5);
+// console.log(array);
+
+// var array = [1, 1, 1, 2, 2, 3, 4, 4, 5, 6];
+
+// let result = [];
+// for (let i = 0; i < array.length; ) {
+//   if (array[i] == array[i + 1]) {
+//     // console.log(array[i]);
+//     // result.push(array[i]);
+//     array.splice(1, 1);
+//   } else {
+//     i++;
+//   }
+// }
+// console.log(array);
