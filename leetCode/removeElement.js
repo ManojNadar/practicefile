7c928968d4d5e71a6edd1ea776a44c25d2949c43
ManojNadar <manojviru03@gmail.com>
2023-06-26 22:54:50 +0530

    update practice

--- leetCode/removeElement.js
+++ leetCode/removeElement.js
@@ -2,11 +2,21 @@ let arr = [3, 2, 2, 3];
 
 let val = 3;
 
-let res = [];
+let count = 0;
+
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] != val) {
+//     res.push(arr[i]);
+//   }
+// }
+
+// console.log(res);
+
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] != val) {
-    res.push(arr[i]);
+  if (arr[i] !== val) {
+    arr[count++] = arr[i];
   }
 }
 
-console.log(res);
+console.log(count);
