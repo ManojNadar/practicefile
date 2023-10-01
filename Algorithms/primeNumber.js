2ef2cd7c502de60daf51c492ab1adfa2b5828769
ManojNadar <manojviru03@gmail.com>
2023-10-01 13:05:16 +0530

    updates

--- Algorithms/primeNumber.js
+++ Algorithms/primeNumber.js
@@ -4,21 +4,16 @@ function isPrime(n) {
   if (n < 2) {
     return false;
   }
-
   for (let i = 2; i < n; i++) {
     if (n % i === 0) {
       return false;
     }
   }
-  //   for (let i = 2; i <= Math.sqrt(n); i++) {
-  //     if (n % i === 0) {
-  //       return false;
-  //     }
-  //   }
+
   return true;
 }
 
-console.log(isPrime(119));
+console.log(isPrime(13));
 
 // Big-O = O(n) - linear
 // as the n value increases the loop increases
